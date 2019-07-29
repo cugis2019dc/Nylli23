1a518c19519707b13e8e49f57c7646b6da0fb673
Kiwi782 <53440819+Kiwi782@users.noreply.github.com>
2019-07-29 13:56:26 -0400

    Add files via upload

--- Day_1.py
+++ Day_1.py
@@ -11,3 +11,28 @@ print(5/2)
 print(5**2)
 print(8/9*3)
 5-2
+5*2
+8/9*3
+
+def multiply (a,b):
+    multiply = a*b 
+    
+    print(multiply)
+    
+multiply(4,5)
+
+
+def add(a,b,c):
+    add = a+b+c
+    
+    print(add)
+    
+
+add(9,8,9)
+
+def triangle(base,height):
+    triangle = 1/2 * base * height
+   
+    print("The area of the triangle with", base ,"and", height ,"is", triangle)
+    
+triangle(12,5)
